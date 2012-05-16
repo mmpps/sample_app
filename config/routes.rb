a44ebafd2700a4ed21d3a066401c906543b6a87b
Matt Maxwell <Matt.Maxwell@sas.com>
2012-05-16 12:36:16 -0400

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 SampleApp::Application.routes.draw do
+  get "sessions/new"
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   get "pages/home"
   get "pages/about"
   get "pages/contact"
 
-  match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
